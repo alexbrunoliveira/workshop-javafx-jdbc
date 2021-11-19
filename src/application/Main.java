f3e8c12c5c06d4bf55f848281cc720f795968372
Alex Bruno de Oliveira <alexbrunoliveira@gmail.com.br>
2021-11-19 14:51:01 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,34 +8,29 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+	private static Scene mainScene;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			
-			// instanciando o objeto fxmlLoader
-			
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			
-			// Comando que faz menu bar preencher a tela
-			
+
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
-			
-			// objeto criado do tipo Scene
-			Scene mainScene = new Scene(scrollPane);
-			
-			// cena principal setada
-			
+
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
-			
-			// mostrando palco
-			
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 
 	public static void main(String[] args) {
